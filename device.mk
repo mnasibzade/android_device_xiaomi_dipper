6cae0dd056b455f586a233083bef8addf17680dc
mnasibzade <euoryexe@gmail.com>
2025-06-07 09:31:19 +0200

    dipper: Stop building XiaomiPocketMode

--- device.mk
+++ device.mk
@@ -101,10 +101,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
